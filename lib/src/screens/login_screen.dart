cd170c83e693fb04a72446fd0362e7d3fd08186a
VictorParodi <victor.parodi.montero@gmail.com>
2019-03-29 07:08:02 -0500

    app1 commit 1: using the From widget and the TextFormField widget

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  Widget build(context) {
+    return Container(
+      margin:EdgeInsets.all(20.0),
+      child: Form(
+        child:Column(
+          children: [
+            emailField(),
+          ]
+        ),
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'youremail@email.com'
+      ),
+    );
+  }
+}
